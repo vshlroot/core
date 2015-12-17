09ff0b99c3910a948e45b7cea93b0aa8376cfe39
Vshlroot <vshlroot@gmail.com>
2015-12-17 18:37:09 +0530

    +randomizedQuickSort() call

--- sort/SortingExample.java
+++ sort/SortingExample.java
@@ -5,15 +5,38 @@ package sort;
  */
 public class SortingExample {
     public static void main(String[] args) {
+
+//        Integer a[]={3,6,2,78,22,1,0,1};
+        Integer a[]={1, 2, 3, 5, 8, 4, 7, 6};
+/*
         // Bubble sort.
         System.out.println("Bubble Sort Example Start");
-        Integer a[]={3,6,2,78,22,1,0,1};
-
         System.out.println("Array before sorting");
-        Sort.printArray(a);
+        Sort.printArray(a,1,"");
         a=Sort.bubbleSort(a);
         System.out.println("Array after sorting");
-        Sort.printArray(a);
+        Sort.printArray(a,1,"");
         System.out.println("Bubble Sort Example End");
+
+
+        // Quick sort.
+        System.out.println("Quick Sort Example Start");
+        System.out.println("Array before sorting");
+        Sort.printArray(a,1,"");
+        Sort.quickSort(a,0,a.length);
+        System.out.println("Array after sorting");
+        Sort.printArray(a,1,"");
+        System.out.println("Quick Sort Example End");
+*/
+            // Randomized Quick sort.
+            System.out.println("Randomized Quick Sort Example Start");
+            System.out.println("Array before sorting");
+            Sort.printArray(a, 1, "");
+            Sort.randomizedQuickSort(a,0,a.length);
+            System.out.println("Array after sorting");
+            Sort.printArray(a,1,"");
+            System.out.println("Randomized Quick Sort Example End");
+
+
     }
 }
