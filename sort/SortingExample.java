2d7bd28b3eb09e69e4a7d4ddf9a3d6843840bbac
Vshlroot <vshlroot@gmail.com>
2015-12-18 22:55:51 +0530

    +mergeSort +merge
    +mergeSort
    merge to merge two separate arrays.

--- sort/SortingExample.java
+++ sort/SortingExample.java
@@ -7,8 +7,9 @@ public class SortingExample {
     public static void main(String[] args) {
 
 //        Integer a[]={3,6,2,78,22,1,0,1};
-        Integer a[]={1, 2, 3, 5, 8, 4, 7, 6};
+
 /*
+        Integer a[]={1, 12, 13, 24, 11,15 , 27, 28};
         // Bubble sort.
         System.out.println("Bubble Sort Example Start");
         System.out.println("Array before sorting");
@@ -27,7 +28,9 @@ public class SortingExample {
         System.out.println("Array after sorting");
         Sort.printArray(a,1,"");
         System.out.println("Quick Sort Example End");
-*/
+
+
+Integer a[]={1, 12, 13, 24, 11,15 , 27, 28};
             // Randomized Quick sort.
             System.out.println("Randomized Quick Sort Example Start");
             System.out.println("Array before sorting");
@@ -37,6 +40,26 @@ public class SortingExample {
             Sort.printArray(a,1,"");
             System.out.println("Randomized Quick Sort Example End");
 
+        // Merge Sort
+        Integer a[]={0,9,29,11,11,1,9,-1};
+        //Integer a[]={11,4};
+        System.out.println("MergeSort Example Start");
+        System.out.println("Array before sorting");
+        Sort.printArray(a, 1, "");
+        //Sort.merge(a, 0, 0, a.length);
+        Sort.mergeSort(a, 0, a.length);
+        System.out.println("Array after sorting");
+        Sort.printArray(a,1,"");
+        System.out.println("MergeSort Example End");
+
+*/
+        // Merge Two sorted arrays.
+        Integer a[]={4,20,7777};
+        Integer b[]={11};
+        Sort.printArray(a,1,"");
+        Sort.printArray(b,1,"");
+        Integer result[]=Sort.merge(a, b);
+        Sort.printArray(result,1,"");
 
     }
 }
